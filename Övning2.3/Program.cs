43ba407d54976e54ee0e4042d840b0bb2c320941
Admin <Admin@TEB127030>
2023-01-27 08:44:18 +0100

    Add project files.

---
+++ Övning2.3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace övning2._3
+{
+
+    class övning2_3
+    {
+
+        static void Main(string[] args)
+        {
+            //300kr för att starta + 500kr för alla dagar efteråt.
+            Console.WriteLine("Hur många dagar ska du hyra bilen?");
+            string tidtext = Console.ReadLine();
+
+            //+1kr för varje kilometer.
+            Console.WriteLine("Hur många kilometer ska du köra den varje dag?");
+            string distanstext = Console.ReadLine();
+
+            double tid = double.Parse(tidtext);
+            double distans = double.Parse(distanstext);
+
+            double total_distans = (distans * tid);
+
+            double kostnad_tid = ((tid * 500) - 200);
+
+            double kostnad_total = (total_distans + kostnad_tid);   
+
+            Console.WriteLine("Hyran kommer då att bli totalt " + kostnad_total + "kr");
+
+        }
+
+    }
+
+}
